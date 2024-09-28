023dd82c3e4c06e211e1409d2636650b3f250e6f
mohamedabdelwahaab <mohammedabdelwahaab170@gmail.com>
2024-09-28 22:37:34 +0300

    authcentication

--- project/src/App.tsx
+++ project/src/App.tsx
@@ -1,16 +1,61 @@
-import React from "react";
-import "./App.css";
-import { Routes, Route } from "react-router-dom";
-import ViewTasks from "./pages/ViewTasks";
+// import React from "react";
+// import "./App.css";
+// import { Routes, Route } from "react-router-dom";
+// import ViewTasks from "./pages/ViewTasks";
+//
+// function App() {
+//   return (
+//     <>
+//       <Routes>
+//         <Route path="/views" element={<ViewTasks />} />
+//       </Routes>
+//     </>
+//   );
+// }
+//
+// export default App;
+import React, { useEffect, useState } from 'react';
+import { SignUp } from './pages/SignUp';
+import { Route, Routes } from "react-router-dom";
+import { Login } from "./pages/Login";
+import { jwtDecode, JwtPayload } from "jwt-decode"; // Use named import
+import './App.css';
 
-function App() {
-  return (
-    <>
-      <Routes>
-        <Route path="/views" element={<ViewTasks />} />
-      </Routes>
-    </>
-  );
+interface UserData extends JwtPayload {
+    // Add any properties expected in your JWT payload
+}
+
+const App: React.FC = () => {
+    const [userData, setUserData] = useState<UserData | null>(null);
+
+    const saveUserData = () => {
+        const token = localStorage.getItem('Token');
+        if (token) {
+            try {
+                const decode = jwtDecode<UserData>(token);
+                setUserData(decode);
+            } catch (error) {
+                console.error("Invalid token", error);
+                setUserData(null);
+            }
+        } else {
+            setUserData(null);
+        }
+    };
+
+    useEffect(() => {
+        saveUserData();
+    }, []);
+
+    return (
+        <div>
+            <Routes>
+                <Route path="signUp" element={<SignUp />} />
+                <Route path="login" element={<Login saveUserData={saveUserData} />} />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
+
